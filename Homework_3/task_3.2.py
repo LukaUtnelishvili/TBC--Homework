af47dcfc96c5660df4548c5ebc5310f7e1b92ee1
LukaUtnelishvili <160949940+LukaUtnelishvili@users.noreply.github.com>
2024-03-05 00:07:17 +0400

    FINISHED THIRD HOMEWORK

---
+++ Homework_3/task_3.2.py
@@ -0,0 +1,8 @@
+import datetime
+y = int(input("Enter your birth year "))
+m = int(input("Enter your birth month "))
+d = int(input("Enter your birth day "))
+birthday = datetime.date(y, m, d)
+datetime.date.today
+birthday_day = birthday.strftime("%A")
+print("You was born on", birthday_day)
